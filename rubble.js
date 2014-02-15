5488122913aa00a9c9a121ead41737f8ecd3a449
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-02-15 00:55:55 -0500

    scroll test

--- rubble.js
+++ rubble.js
@@ -4,4 +4,10 @@
 
 simply.title('Rubble');
 simply.subtitle('This is a subtitle');
-simply.body('This is the body');
+simply.body('This is the body1');
+simply.body('This is the body2');
+simply.body('This is the body3');
+simply.body('This is the body4');
+simply.body('This is the body5');
+simply.body('This is the body6');
+simply.body('This is the body7');
