b4ee28bb22ca95ec469c3f017689ed3fc91b1340
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-02-15 10:59:09 -0500

    Trying to enter/leave post

--- rubble.js
+++ rubble.js
@@ -2,6 +2,14 @@
 // PennApps 2014
 // 02/14/2014
 
+// TO DO:
+
+// Enter post via permalink
+	// scrollable
+
+// Return to list
+	// where left off
+
 simply.title('Rubble');
 
 var reddit_url = 'http://www.reddit.com/r/worldnews/.json';
@@ -27,11 +35,21 @@ var count = parseInt(localStorage.getItem('count')) || 0;
 simply.on('singleClick', function(e) {
 	if (e.button === 'down') {
 		count++;
+		localStorage.setItem('count', count);
+		simply.body(title_array[count]);
+		simply.subtitle(author_array[count]);
 	}
 	else if (e.button === 'up') {
 		count--;
+		localStorage.setItem('count', count);
+		simply.body(title_array[count]);
+		simply.subtitle(author_array[count]);
 	}
-	localStorage.setItem('count', count);
-	simply.body(title_array[count]);
-	simply.subtitle(author_array[count]);
+	else if (e.button === 'select') {
+
+	}
+	else if (e.button == 'back') {
+		
+	}
+	
 });
