270dc4f1cd9436ca3dd6a94b0beb38b6bbb034da
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-02-15 04:00:41 -0500

    Trying to get title7

--- rubble.js
+++ rubble.js
@@ -6,10 +6,10 @@ simply.title('Rubble');
 
 // var count = parseInt(localStorage.getItem('count')) || 0;
 
-// ajax( { url: 'http://reddit.com/.json '}, function(data) {
-// 	var data = JSON.parse(url);
-// 	//simply.body(data.children[0].data.title);
-// });
+ajax( { url: 'http://reddit.com/.json '}, function(data) {
+	var data = JSON.parse(url);
+	//simply.body(data.children[0].data.title);
+});
 
 // simply.on('singleClick', function(e) {
 // 	if (e.button === 'down') {
