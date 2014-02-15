6c17bac9b2f3cda79eb4c64326e8a08c3a552547
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-02-15 18:21:35 -0500

    debugging

--- rubble.js
+++ rubble.js
@@ -57,7 +57,8 @@ simply.on('singleClick', function(e) {
 	}
 	else if (e.button === 'select') {
 		
-		simply.body(first_paragraph_array[count]);
+		simply.body("This is a sanity check");
+		//simply.body(first_paragraph_array[count]);
 
 	}
 	
