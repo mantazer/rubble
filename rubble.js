ef64231f603c264b1b6eeff605213e86d64aae3a
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-02-15 01:55:42 -0500

    JSON test

--- rubble.js
+++ rubble.js
@@ -7,16 +7,18 @@ simply.title('Rubble');
 var count = parseInt(localStorage.getItem('count')) || 0;
 
 $.getJSON( "http://www.reddit.com/.json", function(data) {
-	simply.body(data);
+	$.each(data.data.children, function(i, item) {
+		simply.subtitle(item.data.kind);
+	});
 });
 
-simply.on('singleClick', function(e) {
-	if (e.button === 'down') {
-		simply.subtitle(++count);
-	}
-	else if (e.button === 'up') {
-		simply.subtitle(--count);
-	}
-	localStorage.setItem('count', count);
-});
+// simply.on('singleClick', function(e) {
+// 	if (e.button === 'down') {
+// 		simply.subtitle(++count);
+// 	}
+// 	else if (e.button === 'up') {
+// 		simply.subtitle(--count);
+// 	}
+// 	localStorage.setItem('count', count);
+// });
 
